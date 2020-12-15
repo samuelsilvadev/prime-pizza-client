dcda21811fe6ec9b64ba8fe596947076c97fc9cc
Samuel Silva <samuelsilvawb@gmail.com>
2020-12-15 19:16:37 +0000

    feat: Add SWRConfig on App component in order to reuse with useApi hook

--- src/pages/root/App.tsx
+++ src/pages/root/App.tsx
@@ -1,16 +1,27 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { SWRConfig } from 'swr';
 
 import Home from 'pages/home';
+import Header from 'components/header';
+
+import fetchFactory from 'factories/fetch';
+
+const SWR_CONFIG_VALUE = {
+	fetcher: fetchFactory,
+};
 
 function App() {
 	return (
-		<BrowserRouter>
-			<Switch>
-				<Route path="/">
-					<Home />
-				</Route>
-			</Switch>
-		</BrowserRouter>
+		<SWRConfig value={SWR_CONFIG_VALUE}>
+			<BrowserRouter>
+				<Header />
+				<Switch>
+					<Route path="/">
+						<Home />
+					</Route>
+				</Switch>
+			</BrowserRouter>
+		</SWRConfig>
 	);
 }
 
