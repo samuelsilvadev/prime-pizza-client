890809a1b2cec3572db967dd8ad4bacb41658c6a
Samuel Silva <samuelsilvawb@gmail.com>
2020-12-15 13:09:31 +0000

    chore: Install react-router and config absolute imports

---
+++ src/pages/root/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Home from 'pages/home';
+
+function App() {
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route path="/">
+					<Home />
+				</Route>
+			</Switch>
+		</BrowserRouter>
+	);
+}
+
+export default App;
